90e4b488d82d6645b7f947a3c7410ba4b5df71a4
Abdelrahamn107 <abdelrahmanfangary@gmail.com>
2022-05-04 19:51:45 +0200

    added food reccomendation api

---
+++ lib/models/food_recommendation.dart
@@ -0,0 +1,21 @@
+class FoodRecommendationItem {
+  String? name;
+  double? calories;
+  int? servingSizeG;
+
+  FoodRecommendationItem({this.name, this.calories, this.servingSizeG});
+
+  FoodRecommendationItem.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    calories = json['calories'];
+    servingSizeG = json['serving_size_g'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['calories'] = this.calories;
+    data['serving_size_g'] = this.servingSizeG;
+    return data;
+  }
+}
