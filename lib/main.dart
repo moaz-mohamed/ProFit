9e69b56c7dca41b285df309b436ddab0f1ad15a5
Abdelrahamn107 <abdelrahmanfangary@gmail.com>
2022-05-05 18:09:44 +0200

    changed build.gradle for location error

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,3 @@
-// import 'package:calory_calc/blocs/auth/bloc.dart';
-// import 'package:calory_calc/providers/local_providers/userProvider.dart';
-
-// import 'package:calory_calc/models/dbModels.dart';
-// import 'package:calory_calc/utils/dataLoader.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-//
-// import 'widgets/forms/forms.dart';
-
-// ignore_for_file: unused_label
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
