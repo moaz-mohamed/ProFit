e4337f88b2addc73077b6ab5e66e49f4d87d2eed
[mohamedhashem7] <[mohamedhashem462@gmail.com]>
2022-05-29 07:36:58 +0200

    added logout button

--- lib/widgets/Dashboard/navigation_screen.dart
+++ lib/widgets/Dashboard/navigation_screen.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:profit/main.dart';
 import 'package:profit/services/food_recommendation.dart';
 import 'package:profit/widgets/Dashboard/Screens/Geofencing/geofencing.dart';
 import 'package:profit/widgets/Dashboard/Screens/HomeScreen/first_dashboard.dart';
@@ -11,9 +13,29 @@ import 'package:profit/widgets/Dashboard/Screens/WorkoutScreen/workout_screen.da
 import 'package:profit/widgets/Dashboard/NavigationBloc/tab_bar_bloc.dart';
 import 'package:profit/widgets/Dashboard/NavigationBloc/tab_bar_event.dart';
 import 'package:profit/themes/ThemeUI.dart';
+import 'package:profit/widgets/IntroPage/intro_main.dart';
+import 'package:profit/widgets/IntroPage/Intro/login_screen.dart';
 
-class TabBarPage extends StatelessWidget {
+import 'package:profit/widgets/IntroPage/Intro/login_screen.dart';
+import 'package:profit/widgets/IntroPage/Intro/onboarding_screen.dart';
+
+class TabBarPage extends StatefulWidget {
   const TabBarPage({Key? key}) : super(key: key);
+
+  @override
+  State<TabBarPage> createState() => _TabBarPageState();
+}
+
+class _TabBarPageState extends State<TabBarPage> {
+  checkAuthentication() async {
+    FirebaseAuth.instance.authStateChanges().listen((_user) {
+      if (_user == null) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => MyApp()),
+            (Route<dynamic> route) => false);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +48,42 @@ class TabBarPage extends StatelessWidget {
             appBar: AppBar(
               centerTitle: true,
               title: const Text("ProFit"),
+              actions: [
+                Row(
+                  children: [
+                    Text("logout"),
+                    IconButton(
+                        icon: const Icon(Icons.logout_rounded),
+                        onPressed: () {
+                          _signOut();
+                          FirebaseAuth.instance
+                              .authStateChanges()
+                              .listen((User? user) {
+                            if (user == null) {
+                              print('User is currently signed out!');
+                            } else {
+                              print('User is signed in!');
+                            }
+                          });
+
+                          checkAuthentication();
+                        }),
+                  ],
+                )
+              ],
             ),
-            body: _createBody(context, bloc.currentIndex), //return the index of the screen i want to route
+            body: _createBody(context,
+                bloc.currentIndex), //return the index of the screen i want to route
             backgroundColor: Colors.white,
             bottomNavigationBar: _createdBottomTabBar(context),
           );
         },
       ),
     );
+  }
+
+  Future<void> _signOut() async {
+    await FirebaseAuth.instance.signOut();
   }
 
   Widget _createdBottomTabBar(BuildContext context) {
@@ -80,13 +130,20 @@ class TabBarPage extends StatelessWidget {
       selectedLabelStyle: FitnessAppTheme.navScreen,
       fixedColor: Colors.blue,
       onTap: (index) {
-        bloc.add(NavBarTappedEvent(index: index)); //trigger and notifies the bloc a new event
+        bloc.add(NavBarTappedEvent(
+            index: index)); //trigger and notifies the bloc a new event
       },
     );
   }
 
   Widget _createBody(BuildContext context, int index) {
-    final children = [HomePage(), StepsScreen(), WorkoutsPage(), Geofencing(), FoodRecommendationScreen()];
+    final children = [
+      HomePage(),
+      StepsScreen(),
+      WorkoutsPage(),
+      Geofencing(),
+      FoodRecommendationScreen()
+    ];
     return children[index];
   }
 }
